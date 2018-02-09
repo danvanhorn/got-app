f84c748725c466d96e62e0f49492f4476e66704e
Dan Van Horn <dan.vanhorn17@gmail.com>
2018-02-08 22:38:36 -0800

    added nodemon to handle server updates

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,22 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
+import Characters from '@/components/Characters';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Home',
       component: Home,
     },
+    {
+      path: '/Characters',
+      name: 'Characters',
+      component: Home,
+    }
   ],
 });
